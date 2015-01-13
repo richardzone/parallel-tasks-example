e894de8d015ddee73855ff083d57395f4af4861c
Ziyi, LIU <i@richard.zone>
2015-01-13 03:47:23 +0000

    ziyi: move tagInstance outside

--- generatorParallel.js
+++ generatorParallel.js
@@ -1,19 +1,18 @@
 var co = require('co');
 var R = require('ramda');
 
+var tagInstance = R.curry(function(el, i, all, done) {
+  setTimeout(function() {
+    if(el === 'instance4') {
+      done('error tagging ' + el + ' : Permission denied');
+    } else {
+      console.log('done tagging', el);
+      done(null,el);
+    }
+  }, Math.random() * 2000);
+});
 
 co(function* () {
-
-  var tagInstance = R.curry(function(el, i, all, done) {
-    setTimeout(function() {
-      if(el === 'instance4') {
-        done('error tagging ' + el + ' : Permission denied');
-      } else {
-        console.log('done tagging', el);
-        done(null,el);
-      }
-    }, Math.random() * 2000);
-  });
 
   var instances = [
     'instance-windows-1', 'instance-linux-1', 'instance3', 'instance4'
